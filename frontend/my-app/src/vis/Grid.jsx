a8ce7ca3b67817392b388d1cf2eea66e48bb18de
arnav <arnavgandre.work@gmail.com>
2026-02-22 18:18:48 +0530

    added mobile support sucessfuly

--- frontend/my-app/src/vis/Grid.jsx
+++ frontend/my-app/src/vis/Grid.jsx
@@ -1,20 +1,20 @@
 import * as tf from "@tensorflow/tfjs";
 import { useEffect, useState } from 'react';
-
+import { useRef } from "react";
 
 function Grid({onGridChange}) {
 
+  // state for recognizing drawing or not
   const [isDrawing, setIsDrawing] = useState(false)
-
-    // state for recognizing drawing or not
-
-
-    // grid array to keep track of drawing
-    const [grid, setGrid] = useState(
+  
+  // grid array to keep track of drawing
+  const [grid, setGrid] = useState(
     Array.from({ length: 28 }, () =>
-        Array(28).fill(0)
-    )
-    );
+      Array(28).fill(0)
+  )
+);
+  const containerRef = useRef(null);
+
     // brush define:
 
       const brush = [
@@ -30,68 +30,69 @@ function Grid({onGridChange}) {
   ];
 
     // function to draw
-    function drawCellClick(row, col) {
-        //copy grid
-        const newGrid = grid.map(r => [...r]); 
-        
-        brush.forEach(({dr, dc, value})=>{
-          const r = row + dr;
-          const c = col + dc;
+  function drawCellClick(row, col) {
+      //copy grid
+      const newGrid = grid.map(r => [...r]); 
+      
+      brush.forEach(({dr, dc, value})=>{
+        const r = row + dr;
+        const c = col + dc;
 
-          //bounds check
-          if (r < 0 || r >= 28 || c < 0 || c >= 28) return;
-          newGrid[r][c]= Math.min(1, newGrid[r][c] + value);
-        })
-
-        
-        setGrid(newGrid);
-        onGridChange?.(newGrid);
-        console.log(newGrid)
+        //bounds check
+      if (r < 0 || r >= 28 || c < 0 || c >= 28) return;
+        newGrid[r][c]= Math.min(1, newGrid[r][c] + value);
+      });        
+      setGrid(newGrid);
+      onGridChange?.(newGrid);
+      console.log(newGrid)
     }
 
     // functions called to draw
-    function handleMouseDown(row,col){
+    function handlePointerDown(e){
       setIsDrawing(true);
-      drawCellClick(row,col);
+      handlePointerMove(e);
+      console.log("is drawing now")
 
     }
-    function handleMouseEnter(row,col){
-      if(isDrawing){
+    
+
+    
+    function handlePointerMove(e){
+      if(!isDrawing || !containerRef.current)return;
+
+      const rect = containerRef.current.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      const col = Math.floor(x/15);
+      const row = Math.floor(y / 15);
+
+      if(row >=0 && row<28 && col >=0 && col<28){
         drawCellClick(row,col);
       }
     }
-    function handleMouseUp(){
+    function handlePointerUp(){
       setIsDrawing(false);
     }
 
-    // declaring array to draw
-  const columns = Array.from({ length: 28 }, (_, i) => i);
-  const rows = Array.from({ length: 28 }, (_, i) => i);
-
   return (
 
-  <div>
+  <div
+  ref={containerRef}
+              onPointerDown={handlePointerDown}
+              onPointerMove={handlePointerMove}
+              onPointerUp={handlePointerUp}>
 
     {grid.map((row, rowIndex) => (
-      <div key={rowIndex} style={{ display: "flex" }}>
+      <div key={rowIndex} style={{ display: "flex",
+        
+       }}
+      >
 
         {
-        
           row.map((cell, colIndex) => (
             <div
               key={colIndex}
-              onMouseDown={() => handleMouseDown(rowIndex, colIndex)}
-              onMouseEnter={() => handleMouseEnter(rowIndex,colIndex)}
-              onMouseUp={()=>handleMouseUp()}
-
-              onTouchStart={() => handleMouseDown(rowIndex, colIndex)}
-              onTouchEnd={() => handleMouseEnter(rowIndex,colIndex)}
-              onTouchMove={()=>handleMouseUp()}
-
-              // onPointerDown={handleMouseDown(rowIndex, colIndex)}
-              // onPointerEnter={handleMouseEnter(rowIndex, colIndex)}
-              // onMouseUp={handleMouseUp()}
-
               style={{
                 touchAction: "none",
                 width: 15,
@@ -101,7 +102,9 @@ function Grid({onGridChange}) {
                 userSelect: "none"
               }}
           />
-        ))}
+        )
+                  )
+        }
       </div>
     ))}
   </div>
